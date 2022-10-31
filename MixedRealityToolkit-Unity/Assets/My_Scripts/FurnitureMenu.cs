fbddfcd5b82f48399bc5e0fd8dd1460cd3d08dd5
Bella Palumbi <christabellapalumbi@gmail.com>
2022-10-31 02:15:43 -0400

    Chair, lamp

--- MixedRealityToolkit-Unity/Assets/My_Scripts/FurnitureMenu.cs
+++ MixedRealityToolkit-Unity/Assets/My_Scripts/FurnitureMenu.cs
@@ -10,9 +10,21 @@ public class FurnitureMenu : MonoBehaviour
     public float xPlacementOffset;
     public float zPlacementOffset;
     private float yPos = -1.534f; // TODO: change this to grab y coord of floor plane
+    private float yPos2 = -1.242923f;
+    private float yPos3 = -1.566f;
     
     public void SpawnCouch() { 
         Vector3 pos = new Vector3(gameObject.transform.position.x + xPlacementOffset, yPos, gameObject.transform.position.z + zPlacementOffset);
         GameObject.Instantiate(couchPrefab, pos, Quaternion.identity);
+    }
+
+    public void SpawnChair() { 
+        Vector3 pos = new Vector3(gameObject.transform.position.x + xPlacementOffset, yPos2, gameObject.transform.position.z + zPlacementOffset);
+        GameObject.Instantiate(chairPrefab, pos, Quaternion.identity);
+    }
+
+    public void SpawnLamp() { 
+        Vector3 pos = new Vector3(gameObject.transform.position.x + xPlacementOffset, yPos3, gameObject.transform.position.z + zPlacementOffset);
+        GameObject.Instantiate(lampPrefab, pos, Quaternion.identity);
     }
 }
