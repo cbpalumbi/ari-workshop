9df95507df92b72b9f57ab27d8638923c5d08ef9
Bella Palumbi <christabellapalumbi@gmail.com>
2022-11-28 18:20:19 -0500

    Fade color on hover

--- MixedRealityToolkit-Unity/Assets/My_Scripts/RoomVisualization.cs
+++ MixedRealityToolkit-Unity/Assets/My_Scripts/RoomVisualization.cs
@@ -5,10 +5,37 @@ using Microsoft.MixedReality.Toolkit.Input;
 
 public class RoomVisualization : MonoBehaviour
 {
-    public void OnTouchStarted(HandTrackingInputEventData eventData)
-    {
-        
-        Debug.Log($"Touch started");
+    public Material mat;
+    private bool shouldChangeMat;
+    private MeshRenderer meshRenderer;
+
+    void Start() {
+        meshRenderer = gameObject.GetComponent<MeshRenderer>();
     }
-    public void OnTouchCompleted(HandTrackingInputEventData eventData) {}
+
+    public void VisualizationOnFocusBegin() {
+        shouldChangeMat = true;
+        Debug.Log("hover started");
+        StartCoroutine(TriggerOnHover());
+    }
+
+    public void VisualizationOnFocusEnd() {
+        Debug.Log("hover ended");
+        shouldChangeMat = false;
+        StopCoroutine(TriggerOnHover());
+        meshRenderer.material.SetColor("_Color", new Color(1f, 1f, 1f, 1f));
+
+    }
+
+    IEnumerator TriggerOnHover() {
+        for (int i = 0; i < 10; i++) {
+            if (!shouldChangeMat) {
+                yield break;
+            }
+            Color old = meshRenderer.material.color;
+            Debug.Log("updating color");
+            meshRenderer.material.SetColor("_Color", new Color(old.r, old.g - 0.05f, old.b - 0.05f, old.a));
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 }
